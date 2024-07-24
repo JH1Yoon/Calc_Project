5e7303ef1dd75ad030b9c3c44fc6ad98b6fdb961
JH1Yoon <wlgus4589@naver.com>
2024-07-24 14:05:11 +0900

    Level1 - 1(num1 : 첫 번째 숫자, num2 : 두 번째 숫자)

---
+++ src/calculator/App.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();    // num1 : 첫 번째 숫자
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt(); // num2 : 두 번째 숫자
+
+
+    }
+}
